5d0f0b6ed512940f790279985d7d7cd12aab439c
Griffith Asare Awuah <griffith@dolphtech.com>
2017-02-26 13:41:50 +0000

    Update irc_bot.py

--- irc/irc_bot.py
+++ irc/irc_bot.py
@@ -1,84 +1,56 @@
 import sys
 import socket
-import string
 
-_address = ("irc.freenode.net", 6667)
-buffer = ''
+NICK = "gwuahson"
+IDENT = "thesongwuah"
+REALNAME = "gwuah"
+MASTER = "oxingwuah"
+channel = "#bots"
+
+HOST, PORT = "irc.freenode.net", 6667
+_buffer = ""
+
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-NICK = "griffmaestro"
-INDENT = "griffmaestro"
-REALNAME = "gwuahnode"
-MASTER = "gwuahnode"
-channel = "#bots"
-           
-
-def connect(_address) :
-    irc.connect(_address)
-    return True
-
-def reg_userinfo():
-    irc.send(bytes("NICK %s\r\n" % NICK, "UTF-8"))
-    irc.send(bytes("USER %s %s rat :%s\r\n" % (INDENT, _address[0], REALNAME), "UTF-8"))
-    irc.send(bytes("JOIN %s\r\n" % channel, "UTF-8"));
-    irc.send(bytes("PRIVMSG %s :ogma \r\n" % MASTER, "UTF-8"))
-    return True
-
-
 def recv() :
-    message = buffer + irc.recv(1024).decode("UTF-8")
-    return message
-
-def output(line) :
-    username = chunk[0].split(':')[1]
-    index = b.index("#bots") + 1
-    container = chunk[index:]
-    message = " ".join(word for word in container)
-    message = message[1:]
-    return message
-      
+	return _buffer + irc.recv(1024).decode("UTF-8")
 
 def ping_pong_protocol(chunk) :
-    if (chunk[0] == "PING"):
-        irc.send(bytes("PONG %s\r\n" % chunk[1], "UTF-8"))
-        return True
-    else : pass
+	irc.send(bytes("PONG %s\r\n" % chunk[1], "UTF-8"))
 
 def output(chunk) :
-    username = chunk[0].split(':')[1]
-    index = chunk.index("#bots") + 1
-    container = chunk[index:]
-    message = " ".join(word for word in container)
-    message = message[1:]
-    return (username, message)
+	username = chunk[0].split(':')[1].split('!')[0]
+	index = chunk.index("#bots") + 1
+	container = chunk[index:]
+	message = " ".join(word for word in container)
+	message = message[1:]
+	return (username, message)
 
+def main():
+	irc.connect((HOST, PORT))
+	irc.send(bytes("NICK %s\r\n" % NICK, "UTF-8"))
+	irc.send(bytes("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME), "UTF-8"))
+	irc.send(bytes("JOIN #bots\r\n", "UTF-8"));
+	irc.send(bytes("PRIVMSG %s :Hello \r\n" % MASTER, "UTF-8"))
+	print('Neccesary Info Transmitted\n')
+	while 1 :
+		_buffer = recv()
+		message = _buffer.split("\n")
+		_buffer = message.pop()
 
+		for line in message :
+			line = line.rstrip()
+			line = line.split()
 
+			if (line[0] == "PING") :
+				ping_pong_protocol(line)
 
-def main() :
-    global chunk
-    global username
-    
-    if connect(_address) :
-        if reg_userinfo():
-            while True :
-                buffer = recv()
-                temp = buffer.split("\n")
-                buffer = temp.pop()
-                
-                for line in temp:
-                    line = line.rstrip()
-                    line = line.split()
-                    
-                if (line[0] == "PING") :
-                    ping_pong_protocol(line)
-
-                elif (line[1] == "PRIVMSG") :
-                    username, message = output(line)
-                    print("%s >>> %s" % (username, message))
-
-
-
+			if (line[1] == "PRIVMSG") :
+				username, message = output(line)
+				print("[%s] := %s" % (username, message))
+	else : pass
+	
+main()
 
 
